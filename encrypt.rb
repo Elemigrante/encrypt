7585c03e50bfd0f6fc4103a4a1602c9cc0edb612
Elemigrante <gasdom@yandex.ru>
2019-03-10 13:23:02 +0300

    add support SHA2

--- encrypt.rb
+++ encrypt.rb
@@ -5,7 +5,8 @@ sleep 1
 
 puts 'Каким способом зашифровать:
 1. MD5
-2. SHA1'
+2. SHA1
+3. SHA2'
 
 user_choice = STDIN.gets.chomp
 sleep 1
@@ -13,3 +14,4 @@ sleep 1
 puts 'Вот что получилось:'
 puts Digest::MD5.hexdigest "#{user_input}" if user_choice == '1'
 puts Digest::SHA1.hexdigest "#{user_input}" if user_choice == '2'
+puts Digest::SHA2.hexdigest "#{user_input}" if user_choice == '3'
